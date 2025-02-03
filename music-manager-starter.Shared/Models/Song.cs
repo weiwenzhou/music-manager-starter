f5ebfb888e6a9514607178e4bf84a4760c59fe91
Wei Wen Zhou <weiwen.zhou@stonybrook.edu>
2025-02-02 19:46:43 -0500

    add required to song properties for form validation with data annotations

--- music-manager-starter.Shared/Models/Song.cs
+++ music-manager-starter.Shared/Models/Song.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,17 @@ namespace music_manager_starter.Shared.Models
     public sealed class Song
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Artist is required")]
         public string Artist { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Album is required")]
         public string Album { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Genre is required")]
         public string Genre { get; set; } = string.Empty;
     }
 }
