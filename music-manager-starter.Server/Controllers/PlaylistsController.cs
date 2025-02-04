c8efd325a1086fe754fcf992f3aa469e989b1ea0
Wei Wen Zhou <weiwen.zhou@stonybrook.edu>
2025-02-03 21:28:03 -0500

    create playlist controller with get all playlist and create playlist routes

---
+++ music-manager-starter.Server/Controllers/PlaylistsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using music_manager_starter.Server.Data;
+using music_manager_starter.Shared.Models;
+
+namespace music_manager_starter.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaylistsController : ControllerBase
+    {
+        private readonly DataDbContext _context;
+
+        public PlaylistsController(DataDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] // GET: api/Playlists
+        public async Task<ActionResult<IEnumerable<Playlist>>> GetPlaylists()
+        {
+            return await _context.Playlists.ToListAsync();
+        }
+
+        [HttpPost] // POST: api/Playlists
+        public async Task<ActionResult<Playlist>> PostPlaylist(Playlist playlist)
+        {
+            if (playlist == null)
+            {
+                return BadRequest("Playlist cannot be null.");
+            }
+            _context.Playlists.Add(playlist);
+            await _context.SaveChangesAsync();
+            
+            return Ok();
+        }
+    }
+}
