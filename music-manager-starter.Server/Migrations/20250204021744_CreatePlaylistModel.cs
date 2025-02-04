8306cd419a3c38e39969c48921eee38874cbe9b8
Wei Wen Zhou <weiwen.zhou@stonybrook.edu>
2025-02-03 21:18:13 -0500

    create playlist model

---
+++ music-manager-starter.Server/Migrations/20250204021744_CreatePlaylistModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace music_manager_starter.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatePlaylistModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ music-manager-starter.Shared/Models/Playlist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace music_manager_starter.Shared.Models
+{
+    public sealed class Playlist
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+        public List<Song> Songs { get; set; } = new List<Song>();
+    }
+}
